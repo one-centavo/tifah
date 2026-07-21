1fdcb15e99afd4bb70a9424dc5123a4a17a0138f
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-07-21 14:44:15 -0500

    feat(database): add SanitaryRegistrySeeder for INVIMA sanitary registries

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             LaboratorySeeder::class,
+            SanitaryRegistrySeeder::class,
             ConcentrationUnitSeeder::class,
             ContainerSeeder::class,
             ContentUnitSeeder::class,
