accdb25e7666d3a6a7a1be502756660455e878e5
Gustavo Cabrera <gustavoadolfocabrera15@gmail.com>
2026-08-04 10:33:57 -0500

    Bugfix/login localization (#43)
    * feat(auth): add Spanish language files for authentication and validation
    
    * feat(auth): translate login and forgot password views and update tests
    
    * feat(auth): add greeting and footer translations to Spanish translation file

--- lang/es/validation.php
+++ lang/es/validation.php
@@ -5,6 +5,8 @@ return [
     'string' => 'El campo :attribute debe ser una cadena de caracteres.',
     'boolean' => 'El campo :attribute debe ser booleano.',
     'unique' => 'El campo :attribute ya está en uso.',
+    'email' => 'El campo :attribute debe ser una dirección de correo electrónico válida.',
+    'confirmed' => 'La confirmación del campo :attribute no coincide.',
     'min' => [
         'string' => 'El campo :attribute debe tener al menos :min caracteres.',
     ],
@@ -17,5 +19,9 @@ return [
         'description' => 'descripción',
         'is_cold_chain' => 'cadena de frío',
         'is_special_control' => 'control especial',
+        'email' => 'correo electrónico',
+        'password' => 'contraseña',
+        'form.email' => 'correo electrónico',
+        'form.password' => 'contraseña',
     ],
 ];
