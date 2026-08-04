9a2ccd070ee76719941ad9b632455aa6968bb1f1
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-08-03 19:02:50 -0500

    feat(database): add adjusted_movement_id column to inventory_movements table

---
+++ database/migrations/2026_08_04_000154_add_adjusted_movement_id_to_inventory_movements_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('inventory_movements', function (Blueprint $table) {
+            $table->foreignId('adjusted_movement_id')->nullable()->after('lot_id')->constrained('inventory_movements');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('inventory_movements', function (Blueprint $table) {
+            $table->dropForeign(['adjusted_movement_id']);
+            $table->dropColumn('adjusted_movement_id');
+        });
+    }
+};
