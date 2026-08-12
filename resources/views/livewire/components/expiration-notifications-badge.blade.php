586618b0d429d5baca1f95d946dc9cf5a8809f19
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-08-12 15:30:01 -0500

    fix(layout): integrate expiration alert notification bell in app layout header and sidebar

--- resources/views/livewire/components/expiration-notifications-badge.blade.php
+++ resources/views/livewire/components/expiration-notifications-badge.blade.php
@@ -68,17 +68,18 @@ new class extends Component
     }
 }; ?>
 
-<div class="relative inline-flex items-center" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+<div class="relative inline-flex items-center mr-1" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
     <!-- Notification Bell Button -->
     <button
         @click="open = !open"
         type="button"
-        class="relative p-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 focus:outline-none transition ease-in-out duration-150 rounded-full hover:bg-gray-100 dark:hover:bg-gray-700"
+        class="relative p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 transition ease-in-out duration-150"
         title="Alertas de Vencimiento"
         id="expiration-alerts-bell-btn"
     >
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+        <span class="sr-only">Alertas de Vencimiento</span>
+        <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
         </svg>
 
         @if($unreadCount > 0)
